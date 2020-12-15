879287ea63a2bb82ab6a8f467bb60e790690855d
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-14 22:38:45 -0500

    title success

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -3,5 +3,6 @@ import React from 'react'
 export default () => (
   <div className="page">
     <div className="title">React Calculator</div>
+    <div className="calculate">Figure out the calculator</div>
   </div>
 )
