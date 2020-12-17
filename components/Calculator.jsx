457d4ae34a84bb3843e3b6716e1e2e555711c6fa
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-17 17:55:59 -0500

    number one set

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 
 export default () => {
-  
+
   const [operation, setOperation] = useState('+')
-  
+  const [num1, setNum1] = useState('')
 
   return (
     <div className="page">
       <div className="title">React Calculator</div>
-      <input id="number1" type="text" name="number1" 
+      <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
       <select id="operation" name="operation" 
         <option value="+">+</option>
         <option value="-">-</option>
         <option value="*">*</option>
         <option value="/">/</option>
-      </select>
+      </select >
       <input id="number2" type="text" name="number2" 
       <input type="button" value="Click for answer" />
       <input id="answer" type="text" name="answer" />
-    </div>
+    </div >
   )
 }
