ee9f195105a740429995f702eaac6ddd1caf201b
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-17 17:53:22 -0500

    ready to add setState

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 
 export default () => {
-  const [operation, setOperation] = useState('')
+  
+  const [operation, setOperation] = useState('+')
+  
 
   return (
     <div className="page">
       <div className="title">React Calculator</div>
-      <input id="number1" type="text" name="number1" />
-      <select id="operation" onChange="operator()" />
-      <input id="number2" type="text" name="number2" />
-      <input type="button" value="Click for answer" onClick="calculate()" />
+      <input id="number1" type="text" name="number1" 
+      <select id="operation" name="operation" 
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input id="number2" type="text" name="number2" 
+      <input type="button" value="Click for answer" />
       <input id="answer" type="text" name="answer" />
     </div>
   )
