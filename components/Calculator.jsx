adaa1dc89af7dbda3130ee8e7635c9b3fa1e02a0
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-17 18:01:02 -0500

    answer and warning add

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -6,20 +6,24 @@ export default () => {
   const [num1, setNum1] = useState('')
   const [num2, setNum2] = useState('')
   const [answer, setAnswer] = useState('')
+  const [warning, setWarning] = useState('')
+  const updateAnswer = () => {
+    if (Number.isNaN(num1) || Number.isNaN(num2)) {
+      setWarning('Please provide a valid number for both operands')
 
-  return (
-    <div className="page">
-      <div className="title">React Calculator</div>
-      <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
-      <select id="operation" name="operation" onChange={event => setOperation(event.target.value)}>
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-      </select >
-      <input id="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
-      <input type="button" value="Click for answer" />
-      <input id="answer" type="text" name="answer" />
-    </div >
-  )
-}
+      return (
+        <div className="page">
+          <div className="title">React Calculator</div>
+          <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
+          <select id="operation" name="operation" onChange={event => setOperation(event.target.value)}>
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select >
+          <input id="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
+          <input type="button" value="Click for answer" onClick={updateAnswer} />
+          <input id="answer" type="text" name="answer" defaultValue={answer} disabled />
+        </div >
+      )
+    }
