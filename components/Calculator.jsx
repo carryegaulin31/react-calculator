cb4bfb25f119d4b1810c96e9c6a22301cb6e577e
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-17 17:59:30 -0500

    number 2 and answer

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -4,18 +4,20 @@ export default () => {
 
   const [operation, setOperation] = useState('+')
   const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [answer, setAnswer] = useState('')
 
   return (
     <div className="page">
       <div className="title">React Calculator</div>
       <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
-      <select id="operation" name="operation" 
+      <select id="operation" name="operation" onChange={event => setOperation(event.target.value)}>
         <option value="+">+</option>
         <option value="-">-</option>
         <option value="*">*</option>
         <option value="/">/</option>
       </select >
-      <input id="number2" type="text" name="number2" 
+      <input id="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
       <input type="button" value="Click for answer" />
       <input id="answer" type="text" name="answer" />
     </div >
