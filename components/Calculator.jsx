51335c0aecec1df8ae48b22c87d8fe83f06157d0
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-17 19:48:21 -0500

    operations working warning not

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 export default () => {
-
   const [operation, setOperation] = useState('+')
   const [num1, setNum1] = useState('')
   const [num2, setNum2] = useState('')
@@ -10,34 +9,37 @@ export default () => {
   const updateAnswer = () => {
     if (Number.isNaN(num1) || Number.isNaN(num2)) {
       setWarning('Please provide a valid number for both operands')
-    } else {
-      if (operation === '+') {
+    }
+    switch (operation) {
+      case '+':
         setAnswer(Number(num1) + Number(num2))
-        console.log(answer)
-      } else if (operation === '-') {
+        break
+      case '-':
         setAnswer(Number(num1) - Number(num2))
-      } else if (operation === '*') {
+        break
+      case '*':
         setAnswer(Number(num1) * Number(num2))
-      } else if (operation === '/') {
+        break
+      case '/':
         setAnswer(Number(num1) / Number(num2))
-      }
+        break
+      default:
     }
   }
 
-
-      return (
-        <div className="page">
-          <div className="title">React Calculator</div>
-          <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
-          <select id="operation" name="operation" onChange={event => setOperation(event.target.value)}>
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-          </select >
-          <input id="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
-          <input type="button" value="Click for answer" onClick={updateAnswer} />
-          <input id="answer" type="text" name="answer" defaultValue={answer} disabled />
-        </div >
-      )
-    }
+  return (
+    <div className="page">
+      <div className="title">React Calculator</div>
+      <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
+      <select id="operation" name="operation" onChange={event => setOperation(event.target.value)}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input id="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
+      <input type="button" value="=" onClick={updateAnswer} />
+      <input id="answer" type="text" name="answer" defaultValue={answer} disabled />
+    </div>
+  )
+}
