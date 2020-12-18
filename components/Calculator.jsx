6c5627ab1f0a334237dcb91ebaea160d4c931890
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-18 14:22:58 -0500

    id changed to className

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -33,16 +33,16 @@ export default () => {
   return (
     <div className="page">
       <div className="title">React Calculator</div>
-      <input id="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
+      <input cleassName="number1" type="text" name="number1" onChange={event => setNum1(event.target.value)} />
       <select id="operation" name="operation" onChange={event => setOperation(event.target.value)}>
         <option value="+">+</option>
         <option value="-">-</option>
         <option value="*">*</option>
         <option value="/">/</option>
       </select>
-      <input id="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
+      <input className="number2" type="text" name="number2" onChange={event => setNum2(event.target.value)} />
       <input type="button" value="=" onClick={updateAnswer} />
-      <input id="answer" type="text" name="answer" defaultValue={answer} disabled />
+      <input className="answer" type="text" name="answer" defaultValue={answer} disabled />
       <div className="manu">{warning ? `${warning}` : null}</div>
     </div>
   )
